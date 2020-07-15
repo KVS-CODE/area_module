8a4215e474107969ab895c7bfe3907def73558c7
prinspal <anzaanladka@gmail.com>
2020-07-15 23:21:26 +0530

    area

---
+++ area_module/area.py
@@ -0,0 +1,10 @@
+import math 
+def circle_area(radius):
+    return math.pi*radius**2
+def square_area(side):
+    return side**2
+def rectangle_area(length,breadth):
+    return length*breadth
+   # print("Rectange diagonal = " , ((length)**2 +(breadth)**2))**1/2)
+
+
